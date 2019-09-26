384f797304905c7fb2524f4d292291d97a5081cd
shin.lee <shin.lee@athena.com.tw>
2019-09-26 11:51:55 +0800

    [modified]: hover 後泡泡冒不出來，苦惱 from: https://codepen.io/Grsmto/pen/RPQPPB

--- src/js/all.js
+++ src/js/all.js
@@ -1,6 +1,6 @@
-$.getScript("../js/partials/nav.js");
 $.getScript("../js/partials/header.js");
 $.getScript("../js/partials/public.js");
+$.getScript("../js/partials/button.js");
 $.getScript("../js/partials/footer.js");
 <!--執行 Validator-->
 <!--執行 Validator-->
